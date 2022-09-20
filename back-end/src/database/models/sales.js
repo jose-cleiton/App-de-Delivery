2334edafeebcd8dea7700e84ad866029a7e1840f
Marcos Valdecy <mvaldecy11@gmail.com>
2022-09-20 16:48:52 -0300

    feat: sales model

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,46 @@
+const Sales = (sequelize, DataTypes) => {
+    const Sales = sequelize.define('sales', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+        seller_id: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+        total_price: {
+            type: DataTypes.DECIMAL(9,2),
+        },
+        delivery_address: {
+            type: DataTypes.STRING,
+        },
+        delivery_number: {
+            type: DataTypes.STRING,
+        },
+        sale_date: {
+            type: DataTypes.DATE,
+        },
+        status: {
+            type: DataTypes.STRING,
+        }
+    }, {
+        tableName: 'sales',
+        timestamps: false,
+    })
+
+    Sales.associate = (models) => {
+        Sales.belongsToMany(models.Users,
+            { foreignKey: 'user_id', as: 'client'})
+        Sales.belongsToMany(models.Users,
+            { foreignKey: 'seller_id', as: 'seller'})
+    }
+
+    return Sales;
+}
+
+module.exports  = Sales;
