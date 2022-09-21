9f51791ebe227ec12db2e13501ae6164aaa17a13
BaianorASR <reis.adson2@outlook.com>
2022-09-21 12:52:42 -0300

    docker compose

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const environment = process.env.NODE_ENV || "test";
 
@@ -11,15 +11,16 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  dialect: 'mysql',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || "3306",
+  database: `${process.env.MYSQL_DB_NAME || "delivery-app"}${
+    suffix[environment] || suffix.test
+  }`,
+  username: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "group13password",
+  dialect: "mysql",
   dialectOptions: {
-    timezone: 'Z',
+    timezone: "Z",
   },
   logging: false,
 };
