6bf1aee5f3a51718ee40a69339d41ee246ef96d0
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-09-23 00:17:35 -0300

    styles: login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
