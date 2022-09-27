a0f44c43b60146cad9cdf7f30aa5d4607c8617c5
Marcos Valdecy <mvaldecy11@gmail.com>
2022-09-27 17:24:31 -0300

    ignore

--- front-end/src/pages/Customer/CheckoutPage.jsx
+++ front-end/src/pages/Customer/CheckoutPage.jsx
@@ -1,6 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { utualizarCarrinho } from '../../store/carrinho/carrinho.slice';
+
+function removeProduct(cart, productId) {
+  const aux = JSON.parse(JSON.stringify(cart));
+  const newCart = aux.map((item) => {
+    if (item.id === productId) {
+      item.quantity = 0;
+      return item;
+    }
+    return item;
+  });
+  localStorage.setItem('carrinho', JSON.stringify(newCart));
+  return newCart;
+}
+
 function CheckoutPage() {
+  const dispatch = useDispatch();
   const productsCart = JSON.parse(localStorage.getItem('carrinho'));
+  dispatch(utualizarCarrinho(productsCart));
   const cart = productsCart.filter((item) => item.quantity > 0);
+  const total = cart.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
   return (
     <div>
       <h1>CheckoutPage</h1>
@@ -27,9 +46,14 @@ function CheckoutPage() {
         </thead>
         <tbody>
           { cart.map((data, index) => (
-            <tr key={ index }>
-              <td>
-                { index }
+            <tr key={ index + 1 }>
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-item-number-${index + 1}`
+                }
+
+              >
+                { index + 1 }
               </td>
               <td>
                 { data.name }
@@ -38,15 +62,30 @@ function CheckoutPage() {
                 { data.quantity }
               </td>
               <td>
-                { data.price }
+                { Number(data.price).toFixed(2) }
               </td>
               <td>
-                { data.price * data.quantity }
+                { Number(data.price * data.quantity).toFixed(2) }
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={ () => {
+                    const updatedCart = removeProduct(productsCart, data.id);
+                    console.log('chegou');
+                    dispatch(utualizarCarrinho(updatedCart));
+                  } }
+                >
+                  Remover
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div>
+        { Number(total).toFixed(2) }
+      </div>
     </div>
   );
 }
